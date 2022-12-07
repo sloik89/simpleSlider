e4d402b6be055ec18701ab1883d05c1f96386ad6
sloik89 <sloik89@gmail.com>
2022-12-07 18:11:29 +0100

    second responsive slider

---
+++ main.js
@@ -0,0 +1,68 @@
+const chevronRight = document.querySelector('.chevron-right')
+const chevronLeft = document.querySelector('.chevron-left')
+const slider = document.querySelector('.slider')
+const imageNumber = document.querySelectorAll('.image')
+let indexNumber = 1
+chevronLeft.addEventListener('click',() => {
+    console.log('jestem');
+})
+
+const nextSlide = () => {
+    slider.style.transform = `translateX(-${indexNumber * 800}px)`
+     indexNumber++
+}
+const firstSlider = () => {
+     slider.style.transform = `translateX(0px)`
+     indexNumber = 1
+}
+const previousSlide = () => {
+      slider.style.transform = `translateX(-${(indexNumber-2) * 800}px)`
+     indexNumber--
+}
+const lastSlide = () => {
+      slider.style.transform = `translateX(${-(imageNumber.length - 1) * 800}px)`
+     indexNumber = 4
+}
+chevronRight.addEventListener('click',() => {
+    
+   indexNumber < imageNumber.length ? nextSlide() : firstSlider()
+    
+})
+
+chevronLeft.addEventListener('click',() => {
+    indexNumber > 1 ? previousSlide() : lastSlide()
+})
+
+// second carusel
+const caruselSlider = document.querySelector('.carusel-slider')
+let counter = 0
+const prevBtn = document.querySelector('.prev')
+const nextBtn = document.querySelector('.next')
+const caruselIMages = document.querySelectorAll('.carusel-slider img')
+let size = caruselIMages[0].clientWidth
+prevBtn.addEventListener('click',() => {
+   
+    
+    if(counter < caruselIMages.length - 1){
+        counter++
+    }else{
+        counter = 0
+    }
+    caruselIMages.forEach(elem=>elem.parentElement.classList.remove('active'))
+     caruselIMages[counter].parentElement.classList.add('active')
+    caruselSlider.style.transform = `translateX(${-size * counter}px)`
+})
+nextBtn.addEventListener('click',() => {
+    
+    if(counter <= 0){
+        counter = caruselIMages.length - 1
+    }else{
+        counter --
+    }
+caruselSlider.style.transform = `translateX(${-size * counter}px)`
+})
+window.addEventListener('resize', () => {
+    console.log(counter);
+    size = caruselIMages[0].clientWidth
+    caruselSlider.style.transform = `translateX(${-size * counter}px)`
+})
