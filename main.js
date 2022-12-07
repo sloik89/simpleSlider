3776024d750be6365415d7097fd09d0dfdeaa93d
sloik89 <sloik89@gmail.com>
2022-12-07 19:44:50 +0100

    add interval

--- main.js
+++ main.js
@@ -40,15 +40,27 @@ const prevBtn = document.querySelector('.prev')
 const nextBtn = document.querySelector('.next')
 const caruselIMages = document.querySelectorAll('.carusel-slider img')
 let size = caruselIMages[0].clientWidth
+const dotContainer = document.querySelector('.dot-container')
+let interval = null
+
+
 prevBtn.addEventListener('click',() => {
-   
+
     
-    if(counter < caruselIMages.length - 1){
+    if(counter < caruselIMages.length -1 ){
         counter++
     }else{
         counter = 0
     }
+   clearInterval(interval)
+   autoPLaySLider()
     caruselIMages.forEach(elem=>elem.parentElement.classList.remove('active'))
+      const dot = document.querySelectorAll('.slider-dot')
+    console.log(dot[counter]);
+    
+     dot.forEach(dot=>dot.classList.remove('dot-active'))
+     
+     dot[counter].classList.add('dot-active')
      caruselIMages[counter].parentElement.classList.add('active')
     caruselSlider.style.transform = `translateX(${-size * counter}px)`
 })
@@ -59,10 +71,63 @@ nextBtn.addEventListener('click',() => {
     }else{
         counter --
     }
-caruselSlider.style.transform = `translateX(${-size * counter}px)`
+      clearInterval(interval)
+   autoPLaySLider()
+   const dot = document.querySelectorAll('.slider-dot')
+    dot.forEach(dot=>dot.classList.remove('dot-active'))
+   
+    dot[counter].classList.add('dot-active')
+    caruselIMages.forEach(elem=>elem.parentElement.classList.remove('active'))
+    caruselIMages[counter].parentElement.classList.add('active')
+    caruselSlider.style.transform = `translateX(${-size * counter}px)`
 })
 window.addEventListener('resize', () => {
     console.log(counter);
     size = caruselIMages[0].clientWidth
     caruselSlider.style.transform = `translateX(${-size * counter}px)`
 })
+const handleDotSlider = (idx) => {
+   const dot = document.querySelectorAll('.slider-dot')
+     clearInterval(interval)
+   autoPLaySLider()
+    counter = idx
+    dot.forEach(elem=>elem.classList.remove('dot-active'))
+    dot[counter].classList.add('dot-active')
+     console.log(dot[counter]);
+     caruselSlider.style.transform = `translateX(${-size * counter}px)`
+     caruselIMages.forEach(elem=>elem.parentElement.classList.remove('active'))
+    caruselIMages[counter].parentElement.classList.add('active')
+}
+const creatingSLiderDot = () => {
+    for(let i = 0; i < caruselIMages.length ; i++){
+        const div = document.createElement('div')
+        div.className = 'slider-dot'
+        dotContainer.appendChild(div)
+       
+
+    }
+     caruselIMages[counter].parentElement.classList.add('active') 
+     const dot = document.querySelectorAll('.slider-dot')
+     dot.forEach((elem,idx)=>elem.addEventListener('click',()=>handleDotSlider(idx)))
+     dot[counter].classList.add('dot-active')
+}
+creatingSLiderDot()
+const autoPLaySLider = () =>{
+    interval = setInterval(() => {
+         if(counter < caruselIMages.length -1 ){
+        counter++
+    }else{
+        counter = 0
+    }
+    caruselIMages.forEach(elem=>elem.parentElement.classList.remove('active'))
+      const dot = document.querySelectorAll('.slider-dot')
+    console.log(dot[counter]);
+    
+     dot.forEach(dot=>dot.classList.remove('dot-active'))
+     
+     dot[counter].classList.add('dot-active')
+     caruselIMages[counter].parentElement.classList.add('active')
+    caruselSlider.style.transform = `translateX(${-size * counter}px)`
+    },2000)
+}
+autoPLaySLider()
